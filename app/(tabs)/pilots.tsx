72278d2ed12e7a58ba2d1c4782d98b793929ab39
Wiibleyde <nathan@bonnell.fr>
2025-04-25 11:08:39 +0200

    feat: implement driver list with fetching and display functionality in HomeScreen
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- app/(tabs)/pilots.tsx
+++ app/(tabs)/pilots.tsx
@@ -1,11 +1,72 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList, ActivityIndicator, RefreshControl } from 'react-native';
 
 import Box from '@/theme/Box';
+import Text from '@/theme/Text';
+import { useFetchDrivers } from '@/query/hook';
+import { DriverItem } from '@/components/drivers/DriverItem';
 
-export default function PilotsScreen() {
+export default function HomeScreen() {
+
+  const { data, isLoading, refetch, isRefetching } = useFetchDrivers()
+
   return (
-    <Box></Box>
+    <Box style={styles.container}>
+      <Text variant="title" textAlign="center" style={styles.title}>
+        Pilotes F1
+      </Text>
+
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#ee0000" />
+      ) : (
+        <FlatList
+          refreshControl={
+            <RefreshControl
+              refreshing={isRefetching}
+              onRefresh={refetch}
+              tintColor={"#ee0000"} />
+          }
+          //Get the 20 first drivers ordered by driver_number
+          data={data?.slice(0, 20).sort((a, b) => a.driver_number - b.driver_number)}
+          renderItem={({ item }) => <DriverItem item={item} />}
+          keyExtractor={(item) => item.broadcast_name}
+          contentContainerStyle={styles.listContainer}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={
+            <Box style={styles.emptyContainer}>
+              <Text style={styles.emptyText}>Aucune course disponible</Text>
+            </Box>
+          }
+        />
+      )}
+    </Box>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+    height: '100%',
+    paddingTop: 50,
+    backgroundColor: '#000000',
+  },
+  title: {
+    color: '#FFFFFF',
+    marginBottom: 20,
+  },
+  listContainer: {
+    width: '100%',
+    paddingBottom: 20,
+  },
+  emptyContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  emptyText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  }
+});
--- query/hook.ts
+++ query/hook.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query";
 
 export type Race = {
     circuit_key: number;
@@ -15,14 +15,53 @@ export type Race = {
     year: number;
 }
 
-export const fecthRacesFromYear = async (year: number): Promise<Race[]> => {
+export type Driver = {
+    broadcast_name: string;
+    country_code: string;
+    driver_number: number;
+    first_name: string;
+    full_name: string;
+    headshot_url: string;
+    last_name: string;
+    meeting_key: number;
+    name_acronym: string;
+    session_key: number;
+    team_colour: string;
+    team_name: string;
+}
+
+const fecthRacesFromYear = async (year: number): Promise<Race[]> => {
     const response = await fetch(`https://api.openf1.org/v1/meetings?year=${year}`)
     return response.json()
+}
+
+const fetchDrivers = async (): Promise<Driver[]> => {
+    const response = await fetch('https://api.openf1.org/v1/drivers')
+    const drivers = await response.json()
+    
+    const sortedDrivers = [...drivers].sort((a, b) => b.session_key - a.session_key)
+    
+    const uniqueDriversMap = new Map<string, Driver>()
+    
+    sortedDrivers.forEach(driver => {
+        if (!uniqueDriversMap.has(driver.broadcast_name)) {
+            uniqueDriversMap.set(driver.broadcast_name, driver)
+        }
+    })
+    
+    return Array.from(uniqueDriversMap.values())
 }
 
 export const useFetchRacesFromYear = (year: number) => {
     return useQuery({
         queryKey: ['year', year],
         queryFn: () => fecthRacesFromYear(year),
+    })
+}
+
+export const useFetchDrivers = () => {
+    return useQuery({
+        queryKey: ['drivers'],
+        queryFn: () => fetchDrivers(),
     })
 }
