47334df328ca6a2fae0010995486532a4f29c0cb
Matsuel <matheolang@icloud.com>
2025-05-15 16:37:11 +0200

    feat: ajouter des composants Dot et Logo pour améliorer l'affichage de l'écran d'accueil et refactoriser le code

---
+++ components/onBoarding/Dot.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Animated, StyleSheet } from 'react-native'
+
+interface DotProps {
+    index: number
+    currentIndex: number
+    dotAnimations: {
+        width: Animated.Value
+        opacity: Animated.Value
+    }[]
+}
+
+const Dot = ({
+    index,
+    currentIndex,
+    dotAnimations
+}: DotProps) => {
+    return (
+        <Animated.View
+            key={index}
+            style={[
+                styles.paginationDot,
+                {
+                    width: dotAnimations[index].width,
+                    opacity: dotAnimations[index].opacity,
+                    backgroundColor: index === currentIndex ? '#FF0000' : 'rgba(255,255,255,0.5)',
+                },
+            ]}
+        />
+    )
+}
+
+export default Dot
+
+const styles = StyleSheet.create({
+    paginationDot: {
+        height: 10,
+        borderRadius: 5,
+        marginHorizontal: 5,
+    }
+})
---
+++ components/onBoarding/Logo.tsx
@@ -0,0 +1,28 @@
+import { logo } from '@/constants/OnBoarding'
+import Box from '@/theme/Box'
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
+
+
+const Logo = () => {
+    return (
+        <Box style={styles.logoContainer}>
+            <Image source={logo} style={styles.logo} resizeMode="contain" />
+        </Box>
+    )
+}
+
+export default Logo
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        position: 'absolute',
+        top: 60,
+        left: 20,
+        zIndex: 10,
+    },
+    logo: {
+        width: 110,
+        height: 40,
+    }
+})
