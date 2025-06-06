06e07f135d14deaeeb34083faee5e047c8a04a0d
Wiibleyde <nathan@bonnell.fr>
2025-06-06 10:40:51 +0200

    fix: remove console log from fetchDriverByNumber function
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- query/hook.ts
+++ query/hook.ts
@@ -224,7 +224,6 @@ export const useFetchRadioBySessionKey = (session_key: string) => {
 };
 
 const fetchDriverByNumber = async (driver_number: number, session_key?: number): Promise<Driver | null> => {
-    console.log('Fetching driver by number:', driver_number, 'Session key:', session_key);
     const response = await fetch(`https://api.openf1.org/v1/drivers?driver_number=${driver_number}${session_key ? `&session_key=${session_key}` : ''}`);
     const drivers: Driver[] = await response.json();
     return drivers.length > 0 ? drivers[0] : null;
